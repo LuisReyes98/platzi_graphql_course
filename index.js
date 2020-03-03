d7661b21fce1187480d0cd8a8e89b5760c171b00
Luis Rogelio Reyes <luisrogelio98@gmail.com>
2020-03-03 11:25:17 -0400

    server web y custom types

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
+
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
+const app = express()
+
+const port = process.env.port || 3000
 
 // definiendo el esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
+// se declara en que url de nuestra api se ejecutara el middleware
+app.use('/api', gqlMiddleware({
+  schema: schema, // el schema de graphql
+  rootValue: resolvers, // los resolvers que declaramos antes
+  graphiql: true // activar entorno de desarrollo
+
+}))
+
+app.listen(
+  port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`)
   }
-`)
+)
 
 // Ejecutar el query hello
-graphql(schema, '{ hello }').then( data => {
-  console.log(data)
-})
-
+// graphql(schema, '{ saludo }', resolvers).then( data => {
+//   console.log(data)
+// })
